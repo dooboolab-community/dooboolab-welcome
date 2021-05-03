f0a470e8db2c7b33569a059ca99d85f4b1ea71cd
hyochan <dooboolab@gmail.com>
2021-05-03 14:38:57 +0900

    Release 1.3.0

--- bin/hello.js
+++ bin/hello.js
@@ -5,4 +5,13 @@ const welcome = `
  _| _  _ |_  _  _ | _ |_ 
 (_|(_)(_)|_)(_)(_)|(_||_)
 `;
+
+const homepage = `
+Designed by dooboolab.com
+`;
+
+// eslint-disable-next-line no-console
 console.log('\x1b[92m', welcome);
+
+// eslint-disable-next-line no-console
+console.log('\x1b[92m', homepage);
